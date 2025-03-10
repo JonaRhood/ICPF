633461fdbc7b1f2d7942a28fde7f4058a396893a
Jonathan Cano <jonathancanofreta@gmail.com>
2025-03-10 16:14:21 +0100

    Added pagination logic

--- src/assets/js/library.js
+++ src/assets/js/library.js
@@ -1,32 +1,38 @@
 export const library = () => {
     console.log("Library")
     const insideDivLibrary = document.querySelector("#insideDivLibrary");
+    const divNumbersPagination = document.querySelector("#divNumbersPagination");
 
     let page = 1;
-    let limit = 25;
+    let limit = 24;
     let dataFetch = [];
 
-    const fetchInitialData = async() => {
+    const fetchInitialData = async () => {
         try {
             const response = await fetch("https://icpf-api-production.up.railway.app/libros");
             const data = await response.json();
 
             console.log(data);
             dataFetch = data;
-            console.log(dataFetch);
-            createLibrary(data.slice(0, limit));
+            createLibrary();
+            createPagination();
 
-        } catch(err) {
+        } catch (err) {
             console.log("Error fetching library data", err);
         }
     }
     fetchInitialData();
 
-    const createLibrary = (data) => {
-        data.forEach(Book => {
+    const createLibrary = () => {
+        const dataMax = page * limit;
+        console.log(dataMax);
+        const dataMin = dataMax - limit;
+        console.log(dataMin);
+
+        dataFetch.slice(dataMin, dataMax).forEach(Book => {
             const div = document.createElement("div");
             div.className = "divBooks";
-            
+
             // Image Div
             const divImage = document.createElement("div");
             divImage.className = "divImageBooks";
@@ -36,36 +42,48 @@ export const library = () => {
             img.src = `${Book.libro_imagen}`;
 
             divImage.appendChild(img);
-            
+
             // Details Div
             const divDetails = document.createElement("div");
             divDetails.className = "divDetailsBooks";
 
+            const divTitle = document.createElement("div");
+            divTitle.className = "divTitleBooks";
             const title = document.createElement("span");
             title.className = "bookTitleSpan"
             title.textContent = `${Book.libro_titulo}`;
 
-            divDetails.appendChild(title);
+            divTitle.appendChild(title)
+            divDetails.appendChild(divTitle);
 
             const authorsUl = document.createElement("ul");
             authorsUl.className = "authorsUlBooks"
-            Book.autores.forEach(author => {
+            Book.autores.forEach((author, index) => {
                 const authorLi = document.createElement("li");
                 authorLi.className = "authorLiBooks"
                 authorLi.id = `${author.id}`
                 authorLi.textContent = `${author.nombre} ${author.apellidos}`
+                if (Book.autores.length > 1) {
+                    if (index < Book.autores.length - 1) {
+                        authorLi.textContent += ",";
+                    }
+                }
                 authorsUl.appendChild(authorLi);
             })
 
             divDetails.appendChild(authorsUl);
 
             const categoryUl = document.createElement("ul");
-            categoryUl.className ="categoryUlBooks"
+            categoryUl.className = "categoryUlBooks"
             Book.categorias.forEach(category => {
+                if (category.id === null) {
+                    return;
+                }
                 const categoryLi = document.createElement("li");
                 categoryLi.className = "categoryLiBooks"
                 categoryLi.id = `${category.id}`;
                 categoryLi.textContent = `${category.categoria.toUpperCase()}`;
+                categoryLi.style.backgroundColor = ``
                 categoryUl.appendChild(categoryLi);
             })
 
@@ -75,8 +93,32 @@ export const library = () => {
             div.appendChild(divDetails);
             insideDivLibrary.appendChild(div);
         })
+    };
 
+    const createPagination = () => {
+        const ul = document.createElement("ul");
+        ul.className = "ulPaginationLibrary"
+
+        console.log(dataFetch.length);
+
+        const paginationQuantity = Math.ceil(dataFetch.length / limit) + 1;
+
+        for (let i = 1; i < paginationQuantity; i++) {
+            const li = document.createElement("li");
+            li.dataset.id = i;
+            li.textContent = i;
+            if (page !== i) {
+                li.className = "liPaginationLibrary";
+                li.addEventListener("click", (e) => {
+                    page = e.target.dataset.id;
+                    createLibrary();
+                })
+            }
+            ul.appendChild(li);
+        }
+
+        divNumbersPagination.appendChild(ul);
     }
-    
+
 
 };
